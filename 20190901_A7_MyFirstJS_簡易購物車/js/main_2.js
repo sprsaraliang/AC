2f2683fc53cdf135094ba7ff89c267e2572c4c25
Sara Liang <contact.saraliang@gmail.com>
2019-09-02 13:40:14 +0800

    20190902 已修正_簡易購物車之相關需求

--- 20190901_A7_MyFirstJS_簡易購物車/js/main_2.js
+++ 20190901_A7_MyFirstJS_簡易購物車/js/main_2.js
@@ -9,22 +9,29 @@ const catalog = {
 }
 
 const shoppingCart = [];
-const shippingCost = 140;
-
+const shippingCost = 140;//運費，
 
 var total = 0;
+var pay = 0;
 
 // add items
 function addItemToCart(product){
  total+=catalog[product];
+ priceTopay();
 }
 
+//消費金額沒有到達 $4000 門檻，需要支付運費 $140
+function priceTopay(){
+ if (total <4000){
+  pay=total+shippingCost;
+ }
+}
 
 // print total
 function priceTotal(){
   console.log("Subtotal：$"+total);
   console.log("Added shipping：$"+shippingCost);
-  console.log("Final Price$："+(total +shippingCost));
+  console.log("Final Price$："+pay);
 }
 
 addItemToCart('skirt');
